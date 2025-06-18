374b4adec9d38ba73311c65214763ff51eb966ef
Thanh23-7 <106516270+Thanh23-7@users.noreply.github.com>
2025-06-18 21:54:52 +0700

    Create RootNavigator.tsx

---
+++ navigation/RootNavigator.tsx
@@ -0,0 +1,30 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from '../guest/Login';
+import RegisterScreen from '../guest/Register';
+import ProfileScreen from '../user/Profile';
+import AdminUserManager from '../admin/AdminUserManager';
+import { useAuth } from '../contexts/AuthContext';
+
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  const { currentUser } = useAuth();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {!currentUser ? (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        ) : currentUser.role === 'admin' ? (
+          <Stack.Screen name="AdminUserManager" component={AdminUserManager} />
+        ) : (
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
