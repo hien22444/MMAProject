3a594969e4db62a9ff2363d2ca121a369f8e0f67
hien22444 <trantrinhhien1905@gmail.com>
2025-06-23 23:35:29 +0700

    notthing

--- navigation/RootNavigator.tsx
+++ navigation/RootNavigator.tsx
@@ -9,22 +9,22 @@ import { useAuth } from '../contexts/AuthContext';
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
-  const { currentUser } = useAuth();
+    const { currentUser } = useAuth();
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {!currentUser ? (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-          </>
-        ) : currentUser.role === 'admin' ? (
-          <Stack.Screen name="AdminUserManager" component={AdminUserManager} />
-        ) : (
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {!currentUser ? (
+                    <>
+                        <Stack.Screen name="Login" component={LoginScreen} />
+                        <Stack.Screen name="Register" component={RegisterScreen} />
+                    </>
+                ) : currentUser.role === 'admin' ? (
+                    <Stack.Screen name="AdminUserManager" component={AdminUserManager} />
+                ) : (
+                    <Stack.Screen name="Profile" component={ProfileScreen} />
+                )}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
